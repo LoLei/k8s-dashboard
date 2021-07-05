a72326d58380f50265a60ea8b2be17a95908e8af
Lorenz Leitner <lrnz.ltnr@gmail.com>
2021-07-05 19:41:37 +0200

    Return namespaced pods from api route

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -15,19 +15,45 @@ app.get('/', async (req: Request, res: Response): Promise<Response> => {
   });
 });
 
-app.get('/k8s', async (req: Request, res: Response): Promise<Response> => {
-  const k8sRes = await k8sApi.listNamespacedPod('default');
-  console.log(k8sRes.body);
+app.get('/api/k8s/pods', async (req: Request, res: Response): Promise<Response> => {
+  const k8sRes = await k8sApi.listPodForAllNamespaces();
+
+  const namespacedPods: NamespacedPods = {};
+  k8sRes.body.items.forEach((v1p) => {
+    const pod = {
+      name: v1p.metadata?.name,
+      namespace: v1p.metadata?.namespace,
+      nodeName: v1p.spec?.nodeName,
+      phase: v1p.status?.phase,
+      startTime: v1p.status?.startTime,
+    };
+
+    if (pod.namespace != null) {
+      const podList = namespacedPods[pod.namespace] || [];
+      podList.push(pod);
+      namespacedPods[pod.namespace] = podList;
+    }
+  });
 
   return res.status(200).send({
-    message: 'Hello k8s!',
+    items: namespacedPods,
   });
 });
 
 try {
   app.listen(port, (): void => {
-    console.log(`Connected successfully on port ${port}`);
+    console.log(`Listening on port ${port}`);
   });
 } catch (error) {
   console.error(`Error occured: ${error.message}`);
+}
+
+type NamespacedPods = Record<string, PodResource[]>;
+
+interface PodResource {
+  name?: string;
+  namespace?: string;
+  nodeName?: string;
+  phase?: string;
+  startTime?: Date;
 }
