a0f4f6fc59823ad2144e47470baa9bce53e9d9e1
Lorenz Leitner <lrnz.ltnr@gmail.com>
2021-07-10 20:36:53 +0200

    Add PodDetailsComponent

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { ApiResponsePods, NamespacedPods, PodResource, Node, ApiResponseNodes } from './types';
 import './App.scss';
-import prettyMilliseconds from 'pretty-ms';
 import ObjectComponent from './ObjectComponent';
 import NamespacesComponent from './NamespacesComponent';
 import PodsComponent from './PodsComponent';
+import PodDetailsComponent from './PodDetailsComponent';
 
 function App(): JSX.Element {
   const [namespacedPods, setNamespacedPods] = useState<NamespacedPods>({});
@@ -34,14 +34,6 @@ function App(): JSX.Element {
     getNodesFromApi().then((r) => setNodes(r.topNodes));
   }, []);
 
-  const getAgeInHours = (d: Date | undefined): string => {
-    if (d == null) {
-      return '?';
-    }
-    const diffMs = new Date().getTime() - new Date(d).getTime();
-    return prettyMilliseconds(diffMs, { compact: true });
-  };
-
   return (
     <div className="App">
       <main className="App-main">
@@ -58,48 +50,12 @@ function App(): JSX.Element {
           setSelectedPod={setSelectedPod}
         />
 
-        {selectedPod != null && <h3>Pod Details:</h3>}
-        <div className="pod-details">
-          {selectedPod != null && (
-            <>
-              <div>
-                <b>Name:</b> {selectedPod?.name}
-              </div>
-              <div
-                className={selectedPod?.nodeName === selectedNode?.name ? 'node-selected' : 'node'}
-                onClick={() => setSelectedNode(nodes.find((n) => n.name === selectedPod?.nodeName))}
-              >
-                <b>Node:</b> {selectedPod?.nodeName}
-              </div>
-              <div>
-                <b>Containers:</b>
-                <ul>
-                  {selectedPod?.spec.containerImages?.map((c, idx) => (
-                    <li key={idx}>{c}</li>
-                  ))}
-                </ul>
-              </div>
-              <div>
-                <b>Phase:</b>{' '}
-                <span
-                  className={
-                    selectedPod?.status.phase === 'Running'
-                      ? 'pod-status-phase-running'
-                      : 'pod-status-phase-other'
-                  }
-                >
-                  {selectedPod?.status.phase}
-                </span>
-              </div>
-              <div>
-                <b>Age:</b> {getAgeInHours(selectedPod?.status.startTime)}
-              </div>
-              <div>
-                <b>Restarts:</b> {selectedPod?.status.restartCount}
-              </div>
-            </>
-          )}
-        </div>
+        <PodDetailsComponent
+          selectedPod={selectedPod}
+          selectedNode={selectedNode}
+          nodes={nodes}
+          setSelectedNode={setSelectedNode}
+        />
 
         {selectedNode != null && <h3>Node Details:</h3>}
         {selectedNode != null && (
---
+++ frontend/src/PodDetailsComponent.tsx
@@ -0,0 +1,75 @@
+import prettyMilliseconds from 'pretty-ms';
+import React from 'react';
+import { PodResource, Node } from './types';
+
+const PodDetailsComponent = (props: Props): JSX.Element => {
+  const getAgeInHours = (d: Date | undefined): string => {
+    if (d == null) {
+      return '?';
+    }
+    const diffMs = new Date().getTime() - new Date(d).getTime();
+    return prettyMilliseconds(diffMs, { compact: true });
+  };
+
+  return (
+    <>
+      {props.selectedPod != null && <h3>Pod Details:</h3>}
+      <div className="pod-details">
+        {props.selectedPod != null && (
+          <>
+            <div>
+              <b>Name:</b> {props.selectedPod?.name}
+            </div>
+            <div
+              className={
+                props.selectedPod?.nodeName === props.selectedNode?.name ? 'node-selected' : 'node'
+              }
+              onClick={() =>
+                props.setSelectedNode(
+                  props.nodes.find((n) => n.name === props.selectedPod?.nodeName)
+                )
+              }
+            >
+              <b>Node:</b> {props.selectedPod?.nodeName}
+            </div>
+            <div>
+              <b>Containers:</b>
+              <ul>
+                {props.selectedPod?.spec.containerImages?.map((c, idx) => (
+                  <li key={idx}>{c}</li>
+                ))}
+              </ul>
+            </div>
+            <div>
+              <b>Phase:</b>{' '}
+              <span
+                className={
+                  props.selectedPod?.status.phase === 'Running'
+                    ? 'pod-status-phase-running'
+                    : 'pod-status-phase-other'
+                }
+              >
+                {props.selectedPod?.status.phase}
+              </span>
+            </div>
+            <div>
+              <b>Age:</b> {getAgeInHours(props.selectedPod?.status.startTime)}
+            </div>
+            <div>
+              <b>Restarts:</b> {props.selectedPod?.status.restartCount}
+            </div>
+          </>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default PodDetailsComponent;
+
+interface Props {
+  selectedPod?: PodResource;
+  selectedNode?: Node;
+  nodes: Node[];
+  setSelectedNode: (n?: Node) => void;
+}
