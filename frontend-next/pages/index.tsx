f51da6fabdba32618c740c96bbda41649f54fb3c
Lorenz Leitner <lrnz.ltnr@gmail.com>
2021-07-18 11:42:19 +0200

    Update favicon, change checkbox onClick to onChange
    To remove warning

--- frontend-next/pages/index.tsx
+++ frontend-next/pages/index.tsx
@@ -3,6 +3,7 @@ import { ApiResponseNodes, ApiResponsePods, NamespacedPods, Node } from '../util
 import ContainerNamespacedPodsComponent from '../components/ContainerNamespacedPodsComponent';
 import ContainerNodesComponent from '../components/ContainerNodesComponent';
 import { BiRefresh } from 'react-icons/bi';
+import Head from 'next/head';
 
 export default function Home(): JSX.Element {
   const [namespacedPods, setNamespacedPods] = useState<NamespacedPods>({});
@@ -62,46 +63,51 @@ export default function Home(): JSX.Element {
   };
 
   return (
-    <div className="App">
-      <div className="refresh-container">
-        <button onClick={handleRefreshButtonClicked} disabled={autoRefresh}>
-          <BiRefresh />
-        </button>
-        {' | '}
-        <input type="checkbox" onClick={handleRefreshCheckClicked} checked={autoRefresh} />
-        <span title={`${refreshRateSeconds}s`}>Auto refresh</span>
-        {' | '}
-        <span title="Last updated">
-          Last:{' '}
-          {lastUpdated.toLocaleDateString(undefined, {
-            day: 'numeric',
-            month: 'short',
-            year: 'numeric',
-            hour: 'numeric',
-            minute: 'numeric',
-          })}
-        </span>
-      </div>
-
-      <div className="content-container">
-        <div className="container-namespaced-pods">
-          <ContainerNamespacedPodsComponent namespacedPods={namespacedPods} />
+    <>
+      <Head>
+        <title>k8s-dashboard</title>
+      </Head>
+      <div className="App">
+        <div className="refresh-container">
+          <button onClick={handleRefreshButtonClicked} disabled={autoRefresh}>
+            <BiRefresh />
+          </button>
+          {' | '}
+          <input type="checkbox" onChange={handleRefreshCheckClicked} checked={autoRefresh} />
+          <span title={`${refreshRateSeconds}s`}>Auto refresh</span>
+          {' | '}
+          <span title="Last updated">
+            Last:{' '}
+            {lastUpdated.toLocaleDateString(undefined, {
+              day: 'numeric',
+              month: 'short',
+              year: 'numeric',
+              hour: 'numeric',
+              minute: 'numeric',
+            })}
+          </span>
         </div>
 
-        <div className="container-nodes">
-          <ContainerNodesComponent nodes={nodes} />
-        </div>
-      </div>
+        <div className="content-container">
+          <div className="container-namespaced-pods">
+            <ContainerNamespacedPodsComponent namespacedPods={namespacedPods} />
+          </div>
 
-      <footer>
-        <a href="https://lolei.dev" target="_blank" rel="noreferrer">
-          Home
-        </a>
-        {' | '}
-        <a href="https://github.com/LoLei/k8s-dashboard" target="_blank" rel="noreferrer">
-          Source
-        </a>
-      </footer>
-    </div>
+          <div className="container-nodes">
+            <ContainerNodesComponent nodes={nodes} />
+          </div>
+        </div>
+
+        <footer>
+          <a href="https://lolei.dev" target="_blank" rel="noreferrer">
+            Home
+          </a>
+          {' | '}
+          <a href="https://github.com/LoLei/k8s-dashboard" target="_blank" rel="noreferrer">
+            Source
+          </a>
+        </footer>
+      </div>
+    </>
   );
 }
