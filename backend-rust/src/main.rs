db410f98dc3a8ee7b1a1ea9681f777c1cb71c9c6
Lorenz Leitner <lrnz.ltnr@gmail.com>
2021-07-19 20:36:39 +0200

    Add first version of rust backend

---
+++ backend-rust/src/main.rs
@@ -0,0 +1,59 @@
+#[macro_use]
+extern crate rocket;
+use k8s_openapi::api::core::v1::Pod;
+use kube::api::ListParams;
+use kube::config::KubeConfigOptions;
+use kube::{Api, Client};
+use kube::{Config, ResourceExt};
+use rocket::http::Status;
+use std::convert::TryFrom;
+
+use rocket::serde::json::Json;
+use rocket::serde::{Deserialize, Serialize};
+
+type Id = usize;
+
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct Message {
+    id: Option<Id>,
+}
+
+#[get("/")]
+async fn index() -> Result<Json<Message>, Status> {
+    // TODO: Move this into rocket initialization or somewhere else so it's not done for each request
+    // This should not be necessary in the cluster
+    let config = Config::from_kubeconfig(&KubeConfigOptions {
+        cluster: None,
+        context: None,
+        user: Some(String::from("rust")), // This user had to be created specifically https://github.com/kube-rs/kube-rs/issues/196
+    })
+    .await
+    .unwrap();
+
+    let client = match Client::try_from(config) {
+        // let client = match Client::try_default().await { // This should work in the cluster
+        Ok(c) => c,
+        _ => return Err(Status::InternalServerError),
+    };
+
+    let pods: Api<Pod> = Api::all(client);
+    let lp = ListParams::default();
+
+    for p in match pods.list(&lp).await {
+        Ok(p) => p,
+        Err(e) => {
+            println!("{}", e);
+            return Err(Status::InternalServerError);
+        }
+    } {
+        println!("Found Pod: {}", p.name());
+    }
+
+    Ok(Json(Message { id: Some(4) }))
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
+}
