80e5cb72b974557f3571cdf83464c590ce272afa
Lorenz Leitner <lrnz.ltnr@gmail.com>
2021-07-23 10:35:47 +0200

    Read and return most node values

--- backend-rust/src/lib.rs
+++ backend-rust/src/lib.rs
@@ -1,7 +1,7 @@
 pub mod types;
 
 use anyhow::Context;
-use k8s_openapi::api::core::v1::Pod;
+use k8s_openapi::api::core::v1::{Node, Pod};
 use kube::api::ListParams;
 use kube::config::KubeConfigOptions;
 use kube::{Api, Client};
@@ -9,8 +9,11 @@ use kube::{Config, ResourceExt};
 use rocket::http::Status;
 use std::convert::TryFrom;
 use types::NamespacedPods;
+use types::NodeStatus;
 use types::PodResource;
 use types::Spec;
+
+use crate::types::{NodeInfo, NodeResource};
 
 pub async fn pods() -> Result<NamespacedPods, anyhow::Error> {
     // TODO: Move this into rocket initialization or somewhere else so it's not done for each request
@@ -83,4 +86,63 @@ pub async fn pods() -> Result<NamespacedPods, anyhow::Error> {
     }
 
     Ok(namespaced_pods)
+}
+
+pub async fn nodes() -> Result<Vec<types::Node>, anyhow::Error> {
+    // TODO: Move this into rocket initialization or somewhere else so it's not done for each request
+    // This should not be necessary in the cluster
+    let config = Config::from_kubeconfig(&KubeConfigOptions {
+        cluster: None,
+        context: None,
+        user: Some(String::from("rust")), // This user had to be created specifically https://github.com/kube-rs/kube-rs/issues/196
+    })
+    .await
+    .context(Status::ImATeapot)?; // For demonstration, this status would also be returned in the route if this fails
+
+    let client = Client::try_from(config)?;
+    // let client = Client::try_default().await?; // This should work in the cluster
+
+    let nodes: Api<Node> = Api::all(client);
+    let lp = ListParams::default();
+
+    // TODO: Avoid ITM
+    let mut res_nodes = Vec::<types::Node>::new();
+
+    for n in nodes.list(&lp).await? {
+        let status = match n.status.clone() {
+            Some(x) => x,
+            None => continue,
+        };
+        let node_info = match status.node_info.clone() {
+            Some(x) => x,
+            None => continue,
+        };
+        res_nodes.push(types::Node {
+            name: n.name(),
+            status: NodeStatus {
+                nodeInfo: NodeInfo {
+                    architecture: node_info.architecture,
+                    containerRuntimeVersion: node_info.container_runtime_version,
+                    kernelVersion: node_info.kernel_version,
+                    kubeletVersion: node_info.kubelet_version,
+                    operatingSystem: node_info.operating_system,
+                    osImage: node_info.os_image,
+                },
+            },
+            cpu: NodeResource {
+                // TODO
+                capacity: "".into(),
+                requestTotal: "".into(),
+                limitTotal: "".into(),
+            },
+            memory: NodeResource {
+                // TODO
+                capacity: "".into(),
+                requestTotal: "".into(),
+                limitTotal: "".into(),
+            },
+        });
+    }
+
+    Ok(res_nodes)
 }
--- backend-rust/src/main.rs
+++ backend-rust/src/main.rs
@@ -1,6 +1,6 @@
 #[macro_use]
 extern crate rocket;
-use k8s_dashboard_backend::types::NamespacedPods;
+use k8s_dashboard_backend::types::{NamespacedPods, Node};
 use rocket::http::Status;
 use rocket::serde::json::Json;
 use rocket::serde::{Deserialize, Serialize};
@@ -19,12 +19,19 @@ async fn pods() -> Result<Json<NamespacedPods>, Status> {
         // otherwise the 500 Status is returned
         .map_err(|e| e.downcast().unwrap_or(Status::InternalServerError))?;
 
-    // dbg!(res);
+    Ok(Json(res))
+}
+
+#[get("/api/k8s/nodes")]
+async fn nodes() -> Result<Json<Vec<Node>>, Status> {
+    let res = k8s_dashboard_backend::nodes()
+        .await
+        .map_err(|e| e.downcast().unwrap_or(Status::InternalServerError))?;
 
     Ok(Json(res))
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![pods])
+    rocket::build().mount("/", routes![pods, nodes])
 }
