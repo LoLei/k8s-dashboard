9b654aceef5cd3e629f1ad7f88afd4aa895a8f78
Lorenz Leitner <lrnz.ltnr@gmail.com>
2021-07-24 18:25:14 +0200

    Start of resource_for_pod, but lacks Quantity/Scalar support

--- backend-rust/src/util.rs
+++ backend-rust/src/util.rs
@@ -1,4 +1,10 @@
-use k8s_openapi::api::core::v1::{Node, Pod};
+use std::convert::TryInto;
+use std::convert::TryFrom;
+
+use k8s_openapi::{
+    api::core::v1::{Node, Pod},
+    apimachinery::pkg::api::resource::Quantity,
+};
 use kube::{api::ListParams, Api, Client};
 
 use crate::types::ResourceStatus;
@@ -33,6 +39,26 @@ pub fn memory_for_pod(pod: &Pod) -> ResourceStatus {
 }
 
 fn resource_for_pod(pod: &Pod, resource: &str) -> ResourceStatus {
+    // let req_total: i64 = Quantity("0".into()).try_into().unwrap();
+    // let req_total: i64 = i64::try_from(Quantity("0".into())).unwrap();
+    let limit_total = 0;
+
+    let x = i64::from(false);
+    let int: i32 = "100m".parse().unwrap();
+
+    // TODO: Avoid ITM
+    // TODO: Remove unwrap
+    // TODO: Re-implement quantityToScalar: https://github.com/kubernetes-client/javascript/blob/6b713dc83f494e03845fca194b84e6bfbd86f31c/src/util.ts#L17
+    /*pod.spec.unwrap().containers.iter().for_each(|c| {
+        req_total = req_total
+            + c.resources
+                .unwrap()
+                .requests
+                .get(resource)
+                .unwrap()
+                .to_owned();
+    });*/
+
     // TODO
     ResourceStatus {
         request: 0,
